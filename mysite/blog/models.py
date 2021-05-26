f6f5ee97ed7a745e266fe846d7e7c106ca798699
w40141 <w.40141@gmail.com>
2021-05-26 23:15:03 +0900

    delete secret key

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    """Post class"""
+
+    title = models.CharField(
+        "Title ",
+        max_length=100,
+    )
+    body = models.TextField("Content")
+    created_at = models.DateTimeField(
+        "Date ",
+        auto_now_add=True,
+    )
